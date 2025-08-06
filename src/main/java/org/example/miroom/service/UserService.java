b475e6989e85245d01ae47effff93d6ae1ab387f
qkrtmdalszz <qkrtmdals1212@naver.com>
2025-08-06 11:34:02 +0900

    [add] 회원가입 및 로그인 api추가

--- src/main/java/org/example/miroom/service/UserService.java
+++ src/main/java/org/example/miroom/service/UserService.java
@@ -1,16 +1,47 @@
 package org.example.miroom.service;
 
-
+import org.example.miroom.dto.SignupRequest;
+import org.example.miroom.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.example.miroom.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+
 @Service
+
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(SignupRequest request) {
+
+        if (userRepository.existsByPhoneNumber(request.getPhoneNumber())) {
+            throw new IllegalArgumentException("이미 등록된 전화번호입니다.");
+        }
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("이미 등록된 이메일입니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        User user = new User(
+                request.getNickname(),
+                request.getPhoneNumber(),
+                encodedPassword,
+                request.isAlarmEnabled(),
+                request.getEmail()
+        );
+
+        userRepository.save(user);
+        return user;
     }
 
 }
